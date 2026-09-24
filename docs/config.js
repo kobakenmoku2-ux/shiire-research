e79c008043819904fc02c19a3ac79221c7521d01
Nami <nami@example.local>
2026-09-24 14:47:38 +0900

    OAuthクライアントIDを設定

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 // ここに、セットアップ時に取得した値を入れてください(README参照)
 window.SHIIRE_CONFIG = {
-  CLIENT_ID: 'YOUR_GOOGLE_OAUTH_CLIENT_ID.apps.googleusercontent.com',
+  CLIENT_ID: '1005042588540-buj9qd4s08mi065kmh5kn211skr5etmb.apps.googleusercontent.com',
   SPREADSHEET_ID: '1jUmxSA123iiUQc5SyoXvhgj_StujDLJuf-Us_2JlMj8',
 };
